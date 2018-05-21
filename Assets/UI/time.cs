8130661e22f2a50b1bcc17ea864c0393b2de72ed
GT20176071\user <nkc.shunta.kawai@gmail.com>
2018-05-21 13:43:10 +0900

    UI

---
+++ Assets/UI/time.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time : MonoBehaviour {
+
+    //time変数
+    public float timecount;
+    public Text timelabel;
+    
+	// Use this for initialization
+    void Start () {
+        //制限時間
+        timecount = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       //time表示
+        timelabel.text = string.Format("Time:{0:00}",timecount);
+        //time減少
+        timecount -= Time.deltaTime;
+        //timeループ
+        if (timecount <= 0.0f)
+        {
+            Start();
+
+            //ポイント数
+            FindObjectOfType<score>().countpoint(100);
+        }
+    }
+}
