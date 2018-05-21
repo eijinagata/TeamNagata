1f66ba5c5d829fb6f2ef604a87bdc38b3341ebcd
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-05-21 15:02:07 +0900

    スタート時点から制限時間が０になるまでボールを発射できるように修正

--- Assets/itou/BallCreate.cs
+++ Assets/itou/BallCreate.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallCreate : MonoBehaviour {
-    public GameObject ball;
-
-    Vector3 defaultPos;
+public class BallCreate : MonoBehaviour
+{
+    float time = 0.0f;
+    float shootTime = 0.0f;
+    public GameObject ball; //発射するオブジェクトを覚えておくための変数
+    time timeScript;
+   
 	// Use this for initialization
 	void Start ()
     {
-       
-	}
+        timeScript = FindObjectOfType<time>();
+    }
 	
 	// Update is called once per frame
 	void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    ball.transform.position = transform.position;
+        //    ball.transform.forward = transform.forward;
+        //    Instantiate(ball);
+        //}
+
+        time = timeScript.GetTime();
+        shootTime += Time.deltaTime;
+
+        if (time > 0 && shootTime > 0.5 && Input.GetKeyDown(KeyCode.Space))
         {
+            shootTime = 0.0f;
             ball.transform.position = transform.position;
             ball.transform.forward = transform.forward;
             Instantiate(ball);
