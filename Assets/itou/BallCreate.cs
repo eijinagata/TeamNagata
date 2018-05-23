5a338056ddffb000e92d3ea3ece80e9b69ef4305
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-05-23 14:27:05 +0900

    デバックログを追加

--- Assets/itou/BallCreate.cs
+++ Assets/itou/BallCreate.cs
@@ -34,6 +34,7 @@ public class BallCreate : MonoBehaviour
             ball.transform.position = transform.position;
             ball.transform.forward = transform.forward;
             Instantiate(ball);
+            Debug.Log("発射");
         }
     }
 }
