946051b3a86d685df64e01161366bbb89bf498c1
Eiji Nagata <nkc.eiji.nagata>
2018-05-25 13:42:32 +0900

    UnitLoateteに回転中のフラグのGETアクセサを追加。

--- Assets/Nagata/Script/UnitLotate.cs
+++ Assets/Nagata/Script/UnitLotate.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class UnitLotate : MonoBehaviour {
 
-    public float time;
-
+    public float time;//時間をはかる変数。
+    bool Accessflag = false;//このユニットにマウスが当たっているかどうかのフラグ。
     Quaternion a = Quaternion.AngleAxis(0, Vector3.up);
     Quaternion b = Quaternion.AngleAxis(90, Vector3.up);
 
-    float kakudo = 0f;
-    float degStart = 0f;
-    float degEnd = 90f;
-    float RotlLimit = 90f;
+    float kakudo = 0f;//今の角度
+    float degStart = 0f;//Lerp回転の開始地点。
+    float degEnd = 90f;//Lerp回転の終了地点。
+    float RotlLimit = 90f;//一回のメソッドの実行でこの角度まで回るという角度限界
 
-    bool startRot = false;
-
+    bool startRot = false;//今このユニットが回転中かどうかのフラグ
+    public bool GetAccessflag()//今回転中かのフラグのゲットアクセサ
+    {
+        return startRot;
+    }
     public void RotateCom()
     {
-        if (Input.GetMouseButtonDown(0) && !startRot)
+        if (Input.GetMouseButtonDown(0) && !startRot&&Accessflag==true)
         {
             startRot = true;
         }
 
         if (startRot)
         {
-            time += Time.deltaTime;
-            kakudo = Mathf.Lerp(degStart, degEnd, time * 5f);
+            time += Time.deltaTime;//timeにdeltaTimeを加算
+            kakudo = Mathf.Lerp(degStart, degEnd, time * 5f);//degStart地点からdegEnd地点まで時間×～倍速で回転させる。
             transform.rotation = Quaternion.AngleAxis(kakudo, Vector3.up);
             if (kakudo >= RotlLimit)
             {
@@ -37,6 +40,14 @@ public class UnitLotate : MonoBehaviour {
                 RotlLimit += 90;
             }
         }
+    }
+    public void Access()//マウスが一本道のコライダーに入った時
+    {
+        Accessflag = true;
+    }
+    public void Escape()//マウスが一本道のコライダーより離れたとき
+    {
+        Accessflag = false;
     }
     // Use this for initialization
     void Start () {
