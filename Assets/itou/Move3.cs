21c7493c940844de89cf9930308b82c94206ea99
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-05-28 11:53:00 +0900

    Move2の挙動にバグがありまくっていたので新しい挙動用のスクリプトを作成

---
+++ Assets/itou/Move3.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move3 : MonoBehaviour {
+
+    int countr;
+    float distance = 1.0f;
+    float speed = 0.05f;
+    bool moveFlag = false;
+
+    void RayMove()
+    {
+        //Rayの作成　　　　　　　↓Rayを飛ばす原点　　　↓Rayを飛ばす方向
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        //Rayが当たったオブジェクトの情報を入れる箱
+        RaycastHit hit;
+
+        //もしRayにオブジェクトが衝突したら
+        //                  ↓Ray  ↓Rayが当たったオブジェクト ↓距離
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            if (hit.collider.gameObject.tag == "Wall")
+            {
+                moveFlag = true;
+                Debug.Log("壁に当たった");
+                switch(countr)
+                {
+                    case 0:
+                        transform.Rotate(new Vector3(0.0f, 90.0f, 0.0f));
+                        distance = distance + 1;
+                        countr++;
+                        break;
+                    case 1:
+                        transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
+                        countr++;
+                        break;
+                    case 2:
+                        Destroy(gameObject);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                distance = 1;
+                countr = 0;
+                moveFlag = false;
+            }
+        }
+        else
+        {
+            distance = 1;
+            countr = 0;
+            moveFlag = false;
+            Debug.Log("初期化させています");
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.green, 1, true);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RayMove();
+        //if(moveFlag==false)
+        transform.position += transform.TransformDirection(Vector3.forward) * speed;
+    }
+}
