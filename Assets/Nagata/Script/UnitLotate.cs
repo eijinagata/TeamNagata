90f098696e5dee684fc8262ecb69b25bb0dd414a
Eiji Nagata <nkc.eiji.nagata>
2018-06-01 10:01:24 +0900

    モジュールの回転スピードを外部から変更できるように変数をpublic化

--- Assets/Nagata/Script/UnitLotate.cs
+++ Assets/Nagata/Script/UnitLotate.cs
@@ -13,24 +13,27 @@ public class UnitLotate : MonoBehaviour {
     float degStart = 0f;//Lerp回転の開始地点。
     float degEnd = 90f;//Lerp回転の終了地点。
     float RotlLimit = 90f;//一回のメソッドの実行でこの角度まで回るという角度限界
+    public float LoteSpeed;
 
-    bool startRot = false;//今このユニットが回転中かどうかのフラグ
+    bool startRot;//今このユニットが回転中かどうかのフラグ
     public bool GetAccessflag()//今回転中かのフラグのゲットアクセサ
     {
         return startRot;
     }
+
     public void RotateCom()
     {
         if (Input.GetMouseButtonDown(0) && !startRot&&Accessflag==true)
         {
             startRot = true;
+
         }
 
         if (startRot)
         {
             time += Time.deltaTime;//timeにdeltaTimeを加算
-            kakudo = Mathf.Lerp(degStart, degEnd, time * 5f);//degStart地点からdegEnd地点まで時間×～倍速で回転させる。
-            transform.rotation = Quaternion.AngleAxis(kakudo, Vector3.up);
+            kakudo = Mathf.Lerp(degStart, degEnd, time * LoteSpeed);//degStart地点からdegEnd地点まで時間×～倍速で回転させる。
+            transform.rotation = Quaternion.AngleAxis(kakudo, Vector3.up);//
             if (kakudo >= RotlLimit)
             {
                 startRot = false;
@@ -41,11 +44,11 @@ public class UnitLotate : MonoBehaviour {
             }
         }
     }
-    public void Access()//マウスが一本道のコライダーに入った時
+    private void Access()//マウスが一本道のコライダーに入った時
     {
         Accessflag = true;
     }
-    public void Escape()//マウスが一本道のコライダーより離れたとき
+    private void Escape()//マウスが一本道のコライダーより離れたとき
     {
         Accessflag = false;
     }
@@ -57,5 +60,5 @@ public class UnitLotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         RotateCom();
-	}
+    }
 }
