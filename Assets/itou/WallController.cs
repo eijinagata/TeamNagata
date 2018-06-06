0c46b8c700ce63d68ef464d483edae115318a128
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-06-06 14:57:32 +0900

    当たってきたらボールを落とす処理を追加

--- Assets/itou/WallController.cs
+++ Assets/itou/WallController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class WallController : MonoBehaviour
 {
     Renderer rend;
+    GameObject hitGameObj;
+    Rigidbody rigidbody;
     float alpha;            //アルファ値をいじる用の変数
     float minAlpha = 0.2f;  //最小の薄さ
     float maxAlpha = 0.6f;  //最大の濃さ
     float speed = 0.005f;   //フィードインフィードアウトのスピード
+    float red, green, blue;
     bool alphaFlag = false;
 
     // Use this for initialization
@@ -17,6 +20,10 @@ public class WallController : MonoBehaviour
         alpha = maxAlpha;
 
         rend = GetComponent<Renderer>();
+
+        red = rend.material.color.r;
+        green = rend.material.color.g;
+        blue = rend.material.color.b;
     }
 
     // Update is called once per frame
@@ -43,15 +50,20 @@ public class WallController : MonoBehaviour
         }
 
         //RGBαを更新
-        rend.material.color = new Color(1f, 0f, 0f, alpha);
+        rend.material.color = new Color(red, green, blue, alpha);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Ball")
         {
-            Destroy(other.gameObject);
-            Debug.Log("ボールが壁に当たって消滅");
+            //ボールが当たったらUseGravityにチェックをいれFreezePositionY以外にチェックをいれる
+            hitGameObj = other.gameObject;
+            rigidbody = hitGameObj.GetComponent<Rigidbody>();
+            rigidbody.useGravity = true;
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionX;
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
         }
     }
 }
