39d5b505efd5a2293c423b25dc3e99fa35385fe0
Eiji Nagata <nkc.eiji.nagata>
2018-06-08 10:19:52 +0900

    デバッグ用のUIを配置

---
+++ Assets/Nagata/Script/DebugUISistem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugUISistem : MonoBehaviour {
+
+    public Text HeatLevelText;
+    public  GameObject unit;
+    UnitLotate heatlevel;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
