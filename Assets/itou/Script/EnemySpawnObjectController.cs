83b35f96a6649a1eeadbe883ab51a689e147389c
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-06-11 14:07:16 +0900

    フィールドのエネミーが全滅したらエネミーを追加する処理を追加

--- Assets/itou/Script/EnemySpawnObjectController.cs
+++ Assets/itou/Script/EnemySpawnObjectController.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class EnemySpawnObjectController : MonoBehaviour {
 
     public GameObject enemy;
+    EnemyMove enemyMove;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        enemyMove = FindObjectOfType<EnemyMove>();	
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (enemyMove.DATE <= 0)
         {
             enemy.transform.position = transform.position;
             enemy.transform.forward = transform.forward;
