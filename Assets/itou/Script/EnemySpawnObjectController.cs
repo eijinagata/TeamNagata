a96a37ccd571ad0b04db5737026e96484155d44c
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-06-11 13:39:58 +0900

    エネミーをスポーンさせるオブジェクト用のスクリプトを作成

---
+++ Assets/itou/Script/EnemySpawnObjectController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnObjectController : MonoBehaviour {
+
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            enemy.transform.position = transform.position;
+            enemy.transform.forward = transform.forward;
+            Instantiate(enemy);
+        }
+    }
+}
