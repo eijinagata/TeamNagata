fb3186cc8a3bf90a65b303ff8c96b5ab9df9d662
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-06-13 13:48:54 +0900

    コメントを追加

--- Assets/itou/Script/EnemySpawnObjectController.cs
+++ Assets/itou/Script/EnemySpawnObjectController.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnObjectController : MonoBehaviour {
-
-    public GameObject enemy;
-    EnemyMove enemyMove;
+public class EnemySpawnObjectController : MonoBehaviour
+{
+    public GameObject enemy;    //生成するオブジェクト
+    EnemyMove enemyMove;        //EnemyMove内のDATEアクセサーを使用したいから宣言
 
 	// Use this for initialization
 	void Start ()
     {
+        //EnemyMoveを取得
         enemyMove = FindObjectOfType<EnemyMove>();	
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //フィールドから敵がいなくなったら１体スポーン
         if (enemyMove.DATE <= 0)
         {
             enemy.transform.position = transform.position;
