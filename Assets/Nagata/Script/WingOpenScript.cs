e254593f88a85a71dee50ad1ab261644ff20c51a
Eiji Nagata <nkc.eiji.nagata>
2018-06-18 13:15:18 +0900

    デモを試験的に導入したかったので、CnemaCeneを導入。

--- Assets/Nagata/Script/WingOpenScript.cs
+++ Assets/Nagata/Script/WingOpenScript.cs
@@ -3,33 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WingOpenScript : MonoBehaviour {
-    Animation WingAnim;
+    Animator WingAnim;
     combo Combo;
     bool wing1;
     bool wing2;
     float Timecount;
+    public int Seekint;
     public GameObject WingControle;
     public GameObject Combocount;
 	// Use this for initialization
 	void Start () {
-        WingAnim = WingControle.GetComponent<Animation>();
+        WingAnim = WingControle.GetComponent<Animator>();
         Combo = Combocount.GetComponent<combo>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Combo.ComboCount() >= 10)
+
+        if (Combo.ComboCount() > 10)
         {
-            wing1 = true;
-            if (wing1 == true)
-            {
-                WingAnim.Play("WingOpen1");
-                Timecount += Time.deltaTime;
-            }
-            if (Timecount >= 0.3)
-            {
-                WingAnim.Play("WingOpen2");
-            }
+           // WingAnim.SetInteger("Seekint", 1);
         }
 	}
 }
