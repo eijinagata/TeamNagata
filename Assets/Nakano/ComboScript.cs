0b1f8814f49e23dd219f1dcb7eebdce68d2c4937
Hiroki Nkano <nkc.hiroki.naknao@gmail.com>
2018-06-21 15:02:00 +0900

    リザルトシーンの作成

---
+++ Assets/Nakano/ComboScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboScript : MonoBehaviour {
+
+    int combo = 50;
+    public Text Combolabel;
+    int count = 0;
+    bool swich = false;
+    float totalTime;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (combo > count)
+        {
+            totalTime += Time.deltaTime * 45;
+            count = (int)totalTime;
+            Combolabel.text = count.ToString();
+        }
+    }
+}
---
+++ Assets/Nakano/ScoreScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    int score = 100;
+    public Text Scorelabel;
+    int count = 0;
+    bool swich = false;
+    float totalTime;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(score>count)
+        {
+            totalTime += Time.deltaTime * 90;
+            count = (int)totalTime;
+            Scorelabel.text = count.ToString();
+        }
+    }
+}
