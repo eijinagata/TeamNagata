73d5a530ac170ed5af1a7b578ba4ced0c6f8751c
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-06-25 10:40:15 +0900

    音を鳴らせるように修正

--- Assets/itou/Script/GoalMove.cs
+++ Assets/itou/Script/GoalMove.cs
@@ -9,12 +9,17 @@ public class GoalMove : MonoBehaviour {
     score Score;
     combo GetCombo;
     public GameObject ScoreCount;//スコアカウントが入る変数。
+    AudioSource audioPlus;
+    AudioSource audioMinus;
 
 	// Use this for initialization
 	void Start ()
     {
         Score = ScoreCount.GetComponent<score>();
         GetCombo = ScoreCount.GetComponent<combo>();
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        audioPlus = audioSources[0];
+        audioMinus = audioSources[1];
     }
 
     // Update is called once per frame
@@ -26,15 +31,15 @@ public class GoalMove : MonoBehaviour {
     {
         if (other.gameObject.tag == "Ball")
         {
+            audioPlus.PlayOneShot(audioPlus.clip);
             Score.AddScore(GetCombo.ComboCount());
             GetCombo.active();
             Destroy(other.gameObject);
-            
-
         }
 
         if (other.gameObject.tag == "Enemy")
         {
+            audioMinus.PlayOneShot(audioMinus.clip);
             Score.SetEnemyGoalScore(3);
             GameObject hitObj = other.gameObject;
             EnemyMove enemyMove = hitObj.GetComponent<EnemyMove>();
