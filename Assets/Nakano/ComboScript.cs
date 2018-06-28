38db992bd54083b7bfa12a220dca2aea6dbdc2fc
Hiroki Nkano <nkc.hiroki.naknao@gmail.com>
2018-06-28 13:37:35 +0900

    クリックでスコア表示を実装

--- Assets/Nakano/ComboScript.cs
+++ Assets/Nakano/ComboScript.cs
@@ -7,10 +7,11 @@ public class ComboScript : MonoBehaviour
 {
     //絶対に使用しないように
     public bool MaxC = false;
-    int combo = 50;
+    int combo = 0;
     public Text Combolabel;
     int count = 0;
     float totalTime;
+    bool swich = true;
     // Use this for initialization
     void Start ()
     {
@@ -20,20 +21,38 @@ public class ComboScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (combo > count)
+        if (Input.GetMouseButtonDown(0))
         {
-            totalTime += Time.deltaTime * 45;
-            count = (int)totalTime;
-            Combolabel.text = count.ToString();
+            Combolabel.text = combo.ToString();
+            MaxC = true;
+            swich = false;
+        }
+        if(combo==0)
+        {
+            Combolabel.text = combo.ToString();
+            MaxC = true;
+            swich = false;
+        }
+        if(swich==true)
+        {
 
-            if(combo<count)
+            if (combo > count)
             {
-                Combolabel.text = combo.ToString();
-                MaxC = true;
-            }
-            if(combo==count)
-            {
-                MaxC = true;
+                totalTime += Time.deltaTime * 45;
+                count = (int)totalTime;
+                Combolabel.text = count.ToString();
+
+                if (combo < count)
+                {
+                    Combolabel.text = combo.ToString();
+                    MaxC = true;
+                }
+
+                if (combo == count)
+                {
+                    MaxC = true;
+                }
+
             }
         }
     }
