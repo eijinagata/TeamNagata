aedbfeed2049def8f8b2614795102443c07115aa
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-07-11 14:34:48 +0900

    1Gコミット_2

---
+++ Assets/Fade/Scripts/FadeTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTest : MonoBehaviour
+{
+    [SerializeField]
+    Fade fade = null;
+
+     void Start()
+    {
+        OnClick();
+    }
+
+    public void OnClick()
+    {
+        fade.FadeIn(1, () =>
+        {
+            fade.FadeOut(1);
+        });
+    }
+}
